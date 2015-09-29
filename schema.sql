9a9b47d3192b380db08b07e5ce3693b2ffbd5389
Philippe-Guillaume Losembe <pglosembe@gmail.com>
2015-09-29 14:51:52 -0400

    Addind integrity constraints

--- schema.sql
+++ schema.sql
@@ -1,32 +1,48 @@
-CREATE TABLE User (
-	id int,
+CREATE TABLE Person(
+	id int NOT NULL,
 	email text,
 	phone text,
 	PRIMARY KEY(id)
 );
 
 CREATE TABLE Area (
-	zip text,
+	zip varchar(10) NOT NULL,
 	school_campus text,
 	PRIMARY KEY(zip)
 );
 
+CREATE TABLE Restaurant (
+	id int NOT NULL,
+	zip varchar(10) NOT NULL,
+	name text,
+	cuisine text,
+	open_now boolean,
+	price_range int, -- same scale as in Proposal
+	website_url text,
+	PRIMARY KEY(id),
+	FOREIGN KEY(zip) REFERENCES Area(zip) -- Is in relationship
+);
+
 CREATE TABLE Proposal (
-	int id,
-	int uid,
-	from_time date,
-	until_time date,
+	id int NOT NULL,
+	uid int,
+	rid int NULL, -- at relationship
+	zip varchar(10), -- in (area) relationship
+	from_time date NOT NULL,
+	until_time date NOT NULL,
 	ideal_size int,
 	minimum_size int,
 	maximum_size int,
 	price_range int, -- 0 to 4, price range only has 5 options
 	pending boolean,
-	FOREIGN KEY(uid) REFERENCES User(id),
-	PRIMARY KEY(id)
+	PRIMARY KEY(id),
+	FOREIGN KEY(uid) REFERENCES Person(id),
+	FOREIGN KEY(rid) REFERENCES Restaurant(id), -- at relationship
+	FOREIGN KEY(zip) REFERENCES Area(zip)
 );
 
 CREATE TABLE Meal (
-	id int,
+	id int NOT NULL,
 	size int,
 	time date,
 	pending boolean,
@@ -34,28 +50,20 @@ CREATE TABLE Meal (
 );
 
 CREATE TABLE Matched (
-	mid int,
-	pid int,
-	PRIMARY KEY(mid)
+	mid int NOT NULL,
+	pid int NOT NULL,
+	PRIMARY KEY(pid, mid),
 	FOREIGN KEY(mid) REFERENCES Meal(id),
-	FOREIGN KEY(pid) REFERENCES Proposal(id),
+	FOREIGN KEY(pid) REFERENCES Proposal(id)
 );
 
 CREATE TABLE Reservation (
-	-- id int,
+	id int,
 	mid int,
+	rid int,
 	size int,
 	time date,
-	PRIMARY KEY(mid),
-	FOREIGN KEY(mid) REFERENCES Meal(id)
-);
-
-CREATE TABLE Restaurant (
-	id int,
-	name text,
-	cuisine text,
-	open_now boolean,
-	price_range int, -- same scale as in Proposal
-	website_url text,
-	PRIMARY KEY(id)
+	PRIMARY KEY(id),
+	FOREIGN KEY(mid) REFERENCES Meal(id), -- This is equivalent to the Placed for relationship
+	FOREIGN KEY(rid) REFERENCES Restaurant(id)
 );
