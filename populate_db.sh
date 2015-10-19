28113770eb9cca45690fa0b1138847d699913b5d
Philippe-Guillaume Losembe <pglosembe@gmail.com>
2015-10-19 14:29:09 -0400

    populate_db specifically for w4111 database

--- populate_db.sh
+++ populate_db.sh
@@ -2,8 +2,8 @@
 c++ -std=c++1y -O2 -o insertify db_import.cpp
 
 # create db and build schema
-createdb $1
-psql --dbname=$1 --file=schema.sql 
+# createdb $1
+# psql --dbname=$1 --file=schema.sql 
 
 for filename in sample_data/*.csv; do
 	./insertify $(basename "$filename" .csv) "$filename" > "$filename".sql
@@ -11,11 +11,11 @@ done
 
 cd sample_data
 # populate db with sample_data
-psql --dbname=$1 --file=../schema.sql
-psql --dbname=$1 --file=person.csv.sql
-psql --dbname=$1 --file=area.csv.sql
-psql --dbname=$1 --file=restaurant.csv.sql
-psql --dbname=$1 --file=proposal.csv.sql
-psql --dbname=$1 --file=meal.csv.sql
-psql --dbname=$1 --file=matched.csv.sql
-psql --dbname=$1 --file=reservation.csv.sql
+psql --dbname=proj1part2 -U pvl2109 -h w4111db1.cloudapp.net --file=../schema.sql
+psql --dbname=proj1part2 -U pvl2109 -h w4111db1.cloudapp.net --file=person.csv.sql
+psql --dbname=proj1part2 -U pvl2109 -h w4111db1.cloudapp.net --file=area.csv.sql
+psql --dbname=proj1part2 -U pvl2109 -h w4111db1.cloudapp.net --file=restaurant.csv.sql
+psql --dbname=proj1part2 -U pvl2109 -h w4111db1.cloudapp.net --file=proposal.csv.sql
+psql --dbname=proj1part2 -U pvl2109 -h w4111db1.cloudapp.net --file=meal.csv.sql
+psql --dbname=proj1part2 -U pvl2109 -h w4111db1.cloudapp.net --file=matched.csv.sql
+psql --dbname=proj1part2 -U pvl2109 -h w4111db1.cloudapp.net --file=reservation.csv.sql
