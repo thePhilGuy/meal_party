0cae0ced108010418334fbb7b24b76b301c72444
Philippe-Guillaume Losembe <pglosembe@gmail.com>
2015-11-15 16:21:31 -0500

    Set up google maps front end in map.html

---
+++ javascript/google_maps.js
@@ -0,0 +1,71 @@
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 0, lng: 0},
+    zoom: 14
+  });
+
+  // Bind geocoder to search bar
+  var geocoder = new google.maps.Geocoder();
+
+  document.getElementById('submit').addEventListener('click', function() {
+    geocodeAddress(geocoder, map);
+  });
+
+  var infoWindow = new google.maps.InfoWindow({map: map});
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+}
+
+function geocodeAddress(geocoder, resultsMap) {
+  var address = document.getElementById('address').value;
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+      resultsMap.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+      });
+
+      // Display bounds around the geocoded area
+      displayBounds(results[0].geometry.bounds, resultsMap);
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
+function displayBounds(bounds, resultsMap) {
+  // Have a global rectangle that gets moved
+    var rectangleOptions = {
+        strokeColor: '#0000ff',
+        strokeOpacity: 0.5,
+        strokeWeight: 3,
+        bounds: bounds
+    }
+    var rectangle = new google.maps.Rectangle(rectangleOptions);
+    rectangle.setMap(resultsMap);
+}
