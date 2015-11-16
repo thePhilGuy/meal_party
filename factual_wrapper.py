023502f8953ae3fac73abb64b4bfd8f173a5fc8c
Philippe-Guillaume Losembe <pglosembe@gmail.com>
2015-11-15 21:31:42 -0500

    Now building set of cuisines

--- factual_wrapper.py
+++ factual_wrapper.py
@@ -3,19 +3,32 @@ from pprint import pprint
 factual = Factual('key', 'secret')
 
 def get_restaurants_by_zip(zip):
+	# returns a set of cuisines, as well as a list of restaurants
+	#		  cuisines, restaurants
+
 	print "asking factual for restaurants in ", zip
+
+	# get data from factual API
 	restaurants = factual.table('restaurants-us')
-	data = restaurants.filters({'postcode':zip}).limit(20).data()
+	data = restaurants.filters({'postcode':zip}).data()
+
+	# return objects
 	results = []
+	cuisines = set()
+
+	# build results
 	for i in xrange(20):
 		restaurant = dict()
 		restaurant['name'] = data[i]['name']
 		restaurant['cuisines'] = data[i]['cuisine']
+		cuisines.update(restaurant['cuisines'])
 		restaurant['zip'] = data[i]['postcode']
 		restaurant['price'] = data[i]['price']
 		results.append(restaurant)
-	pprint(results)
-	return results
+	# pprint(results)
+	print "Set of cuisines in ", zip
+	pprint(cuisines)
+	return cuisines, results
 
 
 
