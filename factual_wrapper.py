3b5f5445cf71284c899a23e99bef35d153b4553a
Philippe-Guillaume Losembe <pglosembe@gmail.com>
2015-11-15 21:07:06 -0500

    Building list of dictionaries result for get_restaurants_by_zip

--- factual_wrapper.py
+++ factual_wrapper.py
@@ -1,10 +1,23 @@
 from factual import Factual
+from pprint import pprint
 factual = Factual('key', 'secret')
 
 def get_restaurants_by_zip(zip):
 	print "asking factual for restaurants in ", zip
 	restaurants = factual.table('restaurants-us')
-	data = restaurants.filters({'postcode':zip}).data()
-	print(data)
+	data = restaurants.filters({'postcode':zip}).limit(20).data()
+	results = []
+	for i in xrange(20):
+		restaurant = dict()
+		restaurant['name'] = data[i]['name']
+		restaurant['cuisines'] = data[i]['cuisine']
+		restaurant['zip'] = data[i]['postcode']
+		restaurant['price'] = data[i]['price']
+		results.append(restaurant)
+	pprint(results)
+	return results
 
-get_restaurants_by_zip('10025');
+
+
+if __name__ == '__main__':
+	get_restaurants_by_zip('10025');
